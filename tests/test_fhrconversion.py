91f88cee5f3cb0a4e6867c5328d7fb3428a85dac
Sam Penrose <spenrose@mozilla.com>
2015-10-13 13:32:34 -0700

    wip; move test data to file

--- tests/test_fhrconversion.py
+++ tests/test_fhrconversion.py
@@ -1,32 +1,15 @@
-import unittest
+import os, unittest
 
 # from moztelemetry import coalesce_by_date, search_extractor
 import moztelemetry as MT
-
-DATA = [
-  {
-    'clientId': 'CLIENT_ID',
-    'creationDate': '2015-10-05T15:55:41.224Z',
-    'payload': {
-      'keyedHistograms': {
-        'SEARCH_COUNTS': {
-          'provider1': {
-            'sum': 6
-          }
-        }
-      },
-      'info': {
-        'reason': 'just because'
-      }
-    }
-   }
-]
 
 
 class Test_coalesce_by_date(unittest.TestCase):
 
     def test_all(self):
-        data_days = MT.coalesce_by_date(DATA)
+        data_path = os.path.dirname(__file__) + '/search_mocks.pretty'
+        mocks = eval(open(data_path).read())
+        data_days = MT.coalesce_by_date(mocks)
         value = data_days['2015-10-05']['org.mozilla.searches.counts'][
             'provider1']
         self.assertEqual(value, 6)
