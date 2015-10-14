b5eed24daac9d736c80af68d0ef04a0e10a4be5e
Sam Penrose <spenrose@mozilla.com>
2015-10-14 10:24:11 -0700

    wip; more test coverage via data

--- tests/test_fhrconversion.py
+++ tests/test_fhrconversion.py
@@ -10,9 +10,14 @@ class Test_coalesce_by_date(unittest.TestCase):
         data_days = MT.coalesce_by_date(mocks)
         counts = data_days['2015-10-05']['org.mozilla.searches.counts']
         self.assertEqual(counts['provider1'], 6)
-        self.failIf('provider-idle-daily' in counts)
 
-        # XXX add data values w/ missing keys
+        self.failIf('ping-idle-daily' in counts)
+        self.failIf('ping-missing-creationDate' in counts)
+        self.failIf('ping-short-creationDate' in counts)
+        self.failIf('provider-no-sum' in counts)
+
+        mocks[1]['clientId'] = 'bad client id'
+        self.assertRaises(ValueError, MT.coalesce_by_date, mocks)
 
 
 if __name__ == '__main__':
